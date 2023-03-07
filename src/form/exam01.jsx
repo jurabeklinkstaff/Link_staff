e2e7c6c996f4f042ecf9a5697b08792892815309
jurabeklinkstaff <jurabek.link.staff@gmail.com>
2023-03-07 10:39:54 +0900

    comment

---
+++ src/form/exam01.jsx
@@ -0,0 +1,72 @@
+// import React from "react";
+// import  ReactDOM  from "react-dom";
+
+// // class Exam01 extends React.Component {
+// //     state = {  } 
+// //     render() { 
+// //         return (
+// //         <div>
+// //             <h1>Salom</h1>
+// //             <form>
+// //                 <p>Enter name</p>
+// //                 <input type="text"  placeholder="Text only"/>
+// //             </form>
+// //         </div>
+
+// //         );
+// //     }
+// // }
+
+
+
+// // class Exam01 extends React.Component {
+// //     constructor(props){
+// //         super (props);
+// //         this.state={username:""}
+// //     };
+
+// //     changeHandle=(event) => {
+// //         this.setState({username: event.target.value});
+// //     }
+    
+
+// //     render() { 
+// //         return (
+// //         <div>
+// //             <h1>Salom {this.state.username}</h1>
+// //             <form>
+// //                 <p>Enter name</p>
+// //                 <input type="text" onChange={this.changeHandle}  placeholder="Text only"/>
+// //             </form>
+// //         </div>
+
+// //         );
+// //     }
+// // }
+
+// class Exam01 extends React.Component {
+//     constructor(props){
+//         super (props);
+//         this.state={username:""}
+//     };
+
+//     changeHandle=(event) => {
+//         this.setState({username: event.target.value});
+//     }
+    
+
+//     render() { 
+//         return (
+//         <div>
+//             <h1>Salom {this.state.username}</h1>
+//             <form>
+//                 <p>Enter name</p>
+//                 <input type="text" onChange={this.changeHandle}  placeholder="Text only"/>
+//             </form>
+//         </div>
+
+//         );
+//     }
+// }
+ 
+// export default Exam01;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const a = <h1>salom</h1>;
+console.dir(a)
+root.render(
+);
+
+
+
